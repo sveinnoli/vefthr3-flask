2ed1b5a2ddb48cceddc9bda2b6625f1ffd4ecf4f
sveinnoli <46562494+sveinnoli@users.noreply.github.com>
2020-01-30 10:34:10 +0000

    verkefni2

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-
 kt_list = [2902003030, 3029032020]
-
+frettir = [[0, "Mbl.is oft notað í forritum sem dæmi!", "Það er nyfundið að mbl.is er meiri oftar notað í sem sýnidæmi í forritun en notað til að lesa fréttir", "höfundur@email.is", "\static\images\mbl.png"],
+           [1, "Eldur kviknaði í skógi utan af reykjavik", "Það var risa eldur um daginn sem kviknaði í skóg utan reykjavik, það var fundið út að strumpar voru á háaum dópum þegar eldurinn kviknaði", "strumpur@email.is", "/static/images/forestfire.jpg"]]
 def getSum(n): 
     sum = 0
     while (n != 0): 
-      
         sum = sum + int(n % 10) 
         n = int(n/10) 
-      
     return sum
 
 @app.route('/')
 def index():
 	return render_template("index.html")
-	#return render_template('kennitala.html', kt=kt_list)
 			   
 @app.route('/kennitala')
 def kennitolur():
@@ -26,9 +23,16 @@ def dversuma(kt):
 	kt_sum = getSum(kt)
 	return render_template('dversuma.html', kt_sum=kt_sum, kt=kt,)
 	
-@app.route('/postur')
-def sida2():
-	return render_template('postur.html')
+#Siða fyrir allar frettirnar
+@app.route('/frettir')
+def postur():
+	return render_template('frettir.html', frettir=frettir)
+
+#Siða fyrir Grein
+@app.route('/frettir/<int:id>')
+def frett(id):
+    frett=frettir[id]
+    return render_template('post.html', frett=frett)
 
 @app.errorhandler(404)
 def page_not_found(e):
